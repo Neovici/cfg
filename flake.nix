0e166de1dd65955474bc4ca91bd89061b374cbb6
Meghea Iulian <iulian.meghea@gmail.com>
2021-06-22 17:17:04 +0300

    feat(patch-playwright): implement patch-playwright utility
    implement patch-playwright utility to patch playwright executables (chrome and firefox) on nixos

--- flake.nix
+++ flake.nix
@@ -9,15 +9,18 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         deps = with pkgs; [ nodejs-slim nodePackages.npm python ];
+        env = ''
+          export PATH=$PATH:$(npm bin)
+          export NIXPKGS_ALLOW_UNFREE=1
+          export XDG_DATA_DIRS=$XDG_DATA_DIRS:/etc/profiles/per-user/$USER/share
+        '';
       in
       rec {
         packages = flake-utils.lib.flattenTree (with pkgs;  rec {
           devShell = mkShell {
             buildInputs = deps;
             shellHook = ''
-              export PATH=$PATH:$(npm bin)
-              export NIXPKGS_ALLOW_UNFREE=1
-              export XDG_DATA_DIRS=$XDG_DATA_DIRS:/etc/profiles/per-user/$USER/share
+              ${env}
               tmux-ui() {
                 PROJECT=$(basename $(pwd))
                 tmux at -t $PROJECT || SHELL=bash tmux new -s $PROJECT -n term \; \
@@ -48,8 +51,20 @@
               chmod +w usr/bin
               ln -sr usr/bin/google-chrome-stable usr/bin/google-chrome
             '';
-            runScript= "npm run";
+            runScript = "npm run";
           };
+          patch-playwright = writeShellScriptBin "patch-playwright" ''
+            path=''${1:-~/.cache/ms-playwright}
+            interpr=$(nix eval --raw 'nixpkgs#glibc')/lib64/ld-linux-x86-64.so.2
+            rpath=${google-chrome.rpath}:${lib.makeLibraryPath [dbus-glib xorg.libXt]}
+            find $path/{chromium,firefox}-*/*/ -executable -type f | while read i; do
+              if ! [[ "$i" == *.so ]]; then
+                  ${patchelf}/bin/patchelf --set-interpreter "$interpr" "$i" || true
+              fi
+              ${patchelf}/bin/patchelf --set-rpath "$rpath" "$i" || true
+            done
+            ${patchelf}/bin/patchelf --set-interpreter "$interpr" $path/ffmpeg-*/ffmpeg-linux
+          '';
         });
         defaultPackage = packages.devShell;
       }
