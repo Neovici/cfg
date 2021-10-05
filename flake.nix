d6b2e3f9d8e2e4d74163be9b548db6d86c253534
Meghea Iulian <iulian.meghea@gmail.com>
2021-10-05 15:56:17 +0300

    feat(androidShell): impl

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "Basic dependencies";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"
+      flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -17,56 +17,45 @@
         '';
       in
       rec {
-        packages = flake-utils.lib.flattenTree (with pkgs;  rec {
-          devShell = mkShell {
-            buildInputs = deps;
-            shellHook = ''
-              ${env}
-              tmux-ui() {
-                PROJECT=$(basename $(pwd))
-                tmux at -t $PROJECT || tmux new -s $PROJECT -n term \; \
-                  send "npm ci" C-m \; splitw -v -p 50 \; \
-                  neww -n tig \; send "tig" C-m \; \
-                  neww -n nvim \; send "nvim" C-m \; \
-                  selectw -t 1\; selectp -t 1 \;
-              }
+        packages = flake-utils.lib.flattenTree
+          (with pkgs;  rec {
+            devShell = mkShell {
+              buildInputs = deps;
+              shellHook = ''
+                ${env}
+                tmux-ui() {
+                  PROJECT=$(basename $(pwd))
+                  tmux at -t $PROJECT || tmux new -s $PROJECT -n term \; \
+                    send "npm ci" C-m \; splitw -v -p 50 \; \
+                    neww -n tig \; send "tig" C-m \; \
+                    neww -n nvim \; send "nvim" C-m \; \
+                    selectw -t 1\; selectp -t 1 \;
+                }
+              '';
+            };
+            testShell = mkShell {
+              buildInputs = deps ++ [ firefox google-chrome geckodriver ];
+            };
+
+            androidShell = mkShell rec {
+              packages = [ jdk8 android-studio ];
+              JAVA_HOME = jdk8.home;
+              _JAVA_AWT_WM_NONREPARENTING = 1;
+            };
+
+            patch-playwright = writeShellScriptBin "patch-playwright" ''
+              path=''${1:-~/.cache/ms-playwright}
+              interpr=$(nix eval --raw 'nixpkgs#glibc')/lib64/ld-linux-x86-64.so.2
+              rpath=${google-chrome.rpath}:${lib.makeLibraryPath [dbus-glib xorg.libXt]}
+              find $path/{chromium,firefox}-*/*/ -executable -type f | while read i; do
+                if ! [[ "$i" == *.so ]]; then
+                    ${patchelf}/bin/patchelf --set-interpreter "$interpr" "$i" || true
+                fi
+                ${patchelf}/bin/patchelf --set-rpath "$rpath" "$i" || true
+              done
+              ${patchelf}/bin/patchelf --set-interpreter "$interpr" $path/ffmpeg-*/ffmpeg-linux
             '';
-          };
-          testShell = mkShell {
-            buildInputs = deps ++ [ firefox google-chrome geckodriver ];
-          };
-          acceptanceEnv = buildFHSUserEnv {
-            name = "acceptance";
-            targetPkgs = pkgs: (with pkgs; deps ++ [
-              jre
-              firefox
-              google-chrome
-              # chromedriver
-              glib
-              nspr
-              nss
-              xorg.libX11
-              xorg.libxcb
-            ]);
-            extraBuildCommands = ''
-              chmod +w usr/bin
-              ln -sr usr/bin/google-chrome-stable usr/bin/google-chrome
-            '';
-            runScript = "npm run";
-          };
-          patch-playwright = writeShellScriptBin "patch-playwright" ''
-            path=''${1:-~/.cache/ms-playwright}
-            interpr=$(nix eval --raw 'nixpkgs#glibc')/lib64/ld-linux-x86-64.so.2
-            rpath=${google-chrome.rpath}:${lib.makeLibraryPath [dbus-glib xorg.libXt]}
-            find $path/{chromium,firefox}-*/*/ -executable -type f | while read i; do
-              if ! [[ "$i" == *.so ]]; then
-                  ${patchelf}/bin/patchelf --set-interpreter "$interpr" "$i" || true
-              fi
-              ${patchelf}/bin/patchelf --set-rpath "$rpath" "$i" || true
-            done
-            ${patchelf}/bin/patchelf --set-interpreter "$interpr" $path/ffmpeg-*/ffmpeg-linux
-          '';
-        });
+          });
         defaultPackage = packages.devShell;
       }
     );
