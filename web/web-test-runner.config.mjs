9f4249fc9589b2982ff170304de9d10cdd1d115b
Meghea Iulian <iulian.meghea@gmail.com>
2022-03-01 07:42:10 +0200

    fix: update test-runner config

--- web/web-test-runner.config.mjs
+++ web/web-test-runner.config.mjs
@@ -6,23 +6,17 @@ export default {
 		playwrightLauncher({ product: 'chromium' }),
 		playwrightLauncher({ product: 'firefox' })
 	],
-	coverage: true,
 	coverageConfig: {
-		report: true,
 		reportDir: 'coverage',
-		nativeInstrumentation: true,
-		exclude: ['test/**/*.js', 'demo/**/*.js', 'ui-helpers/*.js', 'node_modules/**'],
 		threshold: {
 			statements: 70,
-			branches: 60,
-			functions: 79,
+			branches: 70,
+			functions: 50,
 			lines: 70
 		}
 	},
-	testFramework: {
-		config: {
-			ui: 'tdd'
-		}
-	},
-	files: ['test/**/*test.js']
+	files: [
+		'**!(node_modules)/*.test.js'
+	],
+	testFramework: { config: { ui: 'tdd' }},
 };
