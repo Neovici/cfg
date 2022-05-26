26a30d24572460fb0ec1361905f5542e60ba4cf5
Meghea Iulian <iulian.meghea@gmail.com>
2022-05-26 18:20:19 +0300

    feat!: add typescript support

---
+++ eslint/typescript.js
@@ -0,0 +1,12 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/typescript',
+	],
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
+	},
+};
