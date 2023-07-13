5106cf1a625989ca05395bf48a9c01873135bf6e
Meghea Iulian <iulian.meghea@gmail.com>
2023-07-13 15:35:03 +0300

    fix(eslint): warn any

--- eslint/typescript.js
+++ eslint/typescript.js
@@ -8,6 +8,7 @@ module.exports = {
 	rules: {
 		'no-unused-vars': 'off',
 		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-explicit-any': 'warn',
 		'import/group-exports': 'off',
 		'new-cap': 'off',
 		'func-style': 'off'
