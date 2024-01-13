2b3ee38cc61b708642fb65dd68586acb6aa37109
Meghea Iulian <iulian.meghea@gmail.com>
2024-01-13 12:29:44 +0200

    feat: update dedupe

--- web/rollup.mjs
+++ web/rollup.mjs
@@ -2,7 +2,13 @@ export const nodeResolve = {
 	mainFields: ['module', 'jsnext:main', 'main'],
 	extensions: ['.ts', '.mjs', '.js', '.cjs', '.json', '.node'],
 	dedupe: (pkg) =>
-		['@neovici', '@polymer', 'lit', 'haunted', 'i18next'].find((prefix) =>
-			pkg.startsWith(prefix)
-		),
+		[
+			'@neovici',
+			'@polymer',
+			'lit',
+			'lit-html',
+			'haunted',
+			'@pionjs/pion',
+			'i18next',
+		].find((prefix) => pkg.startsWith(prefix)),
 };
