230ba0f26da6b4b6b36f5ec61a69239fa59f471a
Meghea Iulian <iulian.meghea@gmail.com>
2025-03-20 09:08:57 +0200

    feat(lint): update typescript rules

--- eslint/typescript.mjs
+++ eslint/typescript.mjs
@@ -1,18 +1,20 @@
 import tseslint from 'typescript-eslint';
 
-export default [
-	...tseslint.configs.recommended,
-	{
-		name: 'ts-overrides',
-		files: ['**/*.+(ts|tsx)'],
-		rules: {
-			'import/named': 'off',
-			'no-unused-vars': 'off',
-			'@typescript-eslint/no-unused-vars': 'error',
-			'@typescript-eslint/no-explicit-any': 'warn',
-			'import/group-exports': 'off',
-			'new-cap': 'off',
-			'func-style': 'off',
-		},
+export default tseslint.config({
+	files: ['**/*.+(ts|tsx)'],
+	extends: [tseslint.configs.recommended],
+	rules: {
+		'import/named': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				caughtErrors: 'none',
+			},
+		],
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'import/group-exports': 'off',
+		'new-cap': 'off',
+		'func-style': 'off',
 	},
-];
+});
